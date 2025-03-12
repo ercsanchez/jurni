ded6493b168c70205755617342fcffd239b0799c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2025-03-12 14:54:21 +0800

    fixup: feat-next-auth setup with jwt strat ea2107d82700072508a0471e995615abe1bd1c67

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,21 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 // import { neon } from '@neondatabase/serverless';
+
+import { config } from '@/config/app.config';
 // import * as schema from '@/db/schema';
 
 // db connection w/o using neon db serverless
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(config.DATABASE_URL!);
 
 // drizzle synchronous connection: https://orm.drizzle.team/docs/get-started/neon-new#step-3---connect-drizzle-orm-to-the-database
-// const sql = neon(process.env.DATABASE_URL!);
+// const sql = neon(config.DATABASE_URL!);
 // export const db = drizzle({ client: sql });
 
-// if passing the schema
-// const sql = neon(process.env.DATABASE_URL!);
+// if passing the drizzle schema
+// const sql = neon(config.DATABASE_URL!);
 // export const db = drizzle({ client: sql, schema: schema });
 
 // - OR -
 // https://neon.tech/docs/guides/drizzle-migrations#seed-the-database
-// const sql = neon(process.env.DATABASE_URL!);
+// const sql = neon(config.DATABASE_URL!);
 // export const db = drizzle(sql);
