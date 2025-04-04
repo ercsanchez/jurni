4cba8438a1a6bb8e93b86793865c277ee422b2cd
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2025-04-04 18:13:19 +0800

    refactor: improve typing and error handling for httpRes and simplify code for readability and ease of use

--- src/utils/http-response.ts
+++ src/utils/http-response.ts
@@ -1,162 +1,142 @@
+import { DrizzleError } from 'drizzle-orm';
 import { NextResponse } from 'next/server';
 
-interface HttpResConfig {
-  [key: string]: { status: number; success?: string; error?: string };
-}
+const httpResConfig: {
+  [key: string]: {
+    status: number;
+    statusText: string;
+  };
+} = {
+  ok: { status: 200, statusText: 'OK' },
+  created: { status: 201, statusText: 'Created' },
 
-const httpResConfig: HttpResConfig = {
-  ok: { status: 200, success: 'OK' },
-  created: { status: 201, success: 'Created' }, // request has been received but not yet acted upon
-  accepted: { status: 202, success: 'Accepted' },
-  // noContent: { status: 204, success: 'No Content' }, // response body must be empty for 204, so this is not applicable since we always return a { data: {}, success: "" }
-  found: { status: 302, error: 'Found' }, // request to URI is successful then redirect
-  badRequest: { status: 400, error: 'Bad Request' },
-  unauthenticated: { status: 401, error: 'Unauthenticated' }, // user is authenticated
-  forbidden: { status: 403, error: 'Forbidden' }, // user is authenticated but is not allowed to access resource
-  notFound: { status: 404, error: 'Not Found' },
-  conflict: { status: 409, error: 'Conflict' },
-  internalServerErr: { status: 500, error: 'Internal Server Error' },
-  serviceUnavailable: { status: 503, error: 'Service Unavailable' },
-  // networkAuthRequired: {
-  //   status: 511,
-  //   error: 'Network Authentication Required',
-  // }, // additional authentication in server to access another server/service
+  // request has been received but not yet acted upon
+  accepted: { status: 202, statusText: 'Accepted' },
+
+  // response body must be empty for 204, so this is not applicable since we always return a { data: {}, success: "" }
+  // noContent: { status: 204, , statusText: 'No Content' },
+
+  // response body must be empty for 204, so this is not applicable since we always return a { data: {}, success: "" }
+  found: { status: 302, statusText: 'Found' },
+
+  badRequest: { status: 400, statusText: 'Bad Request' },
+
+  // user is unauthenticated
+  unauthenticated: { status: 401, statusText: 'Unauthenticated' },
+
+  // user is authenticated but is not allowed to access resource
+  forbidden: { status: 403, statusText: 'Forbidden' },
+
+  notFound: { status: 404, statusText: 'Not Found' },
+  conflict: { status: 409, statusText: 'Conflict' },
+  internalServerErr: { status: 500, statusText: 'Internal Server Error' },
+  serviceUnavailable: { status: 503, statusText: 'Service Unavailable' },
+
+  // additional authentication in server to access another server/service
+  networkAuthRequired: {
+    status: 511,
+    statusText: 'Network Authentication Required',
+  },
 };
 
 interface SuccessData {
-  data: unknown;
+  data: object | string;
   message?: string;
-  success?: string;
+  headers?: Headers;
 }
 
 interface ErrorData {
   message?: string;
-  error?: string;
+  headers?: Headers;
 }
 
-interface HttpRes {
-  [key: string]: (
-    payload?: SuccessData | ErrorData,
-  ) => NextResponse<SuccessData | ErrorData>;
-  // ) => NextResponse<SuccessResponse | ErrorResponse>; // only passed args need to be specified in the NextResponse generic type
-}
+type HttpResFn = (
+  payload?: SuccessData | ErrorData,
+) => NextResponse<SuccessData | ErrorData>;
+// only passed args need to be specified in the NextResponse generic type
 
-const httpRes: HttpRes = {};
+const httpRes: {
+  // ok: HttpResFn; // no need to define each key
+  [key: string]: HttpResFn;
+} = {
+  // ok: () => NextResponse.json({}, {}), // no need to define the keys
+};
 
 for (const key in httpResConfig) {
-  httpRes[key] = (payload = {}) => {
+  httpRes[key] = (payload?: SuccessData | ErrorData) => {
     const httpResConfigValues = httpResConfig[key];
-    const { status, ...rest } = httpResConfigValues;
+    const { status, statusText } = httpResConfigValues;
 
-    return NextResponse.json(
-      {
-        ...payload,
-        ...rest,
-      },
-      { status },
-    );
+    return httpResByStatus({ status, statusText, ...payload });
   };
 }
 
-type HttpResByStatus = (
-  status: number,
-  statusText?: string,
-) => NextResponse<SuccessData | ErrorData>;
-
-const httpResByStatus: HttpResByStatus = (status, statusText = '') => {
+function httpResByStatus({
+  status,
+  statusText = '',
+  message,
+  data,
+  headers,
+}: {
+  status: number;
+  statusText?: string;
+  message?: string;
+  data?: object | string;
+  headers?: Headers;
+}) {
+  // }): NextResponse<SuccessData | ErrorData> { // no need to specify return type
   const responseArray = Object.entries(httpResConfig);
 
   // filter out the matching httpResConfigKey by comparing the status
-  const result = responseArray
+  const [httpResConfigKey] = responseArray
     .filter((i) => status === i[1].status)
     .map((i) => i[0]);
-  const matchingHttpResConfigKeyFound = result.length === 1;
 
-  // console.log('result====>', result);
-
-  // console.log(
-  //   'matchingHttpResConfigKeyFound====>',
-  //   matchingHttpResConfigKeyFound,
-  // );
-  if (!matchingHttpResConfigKeyFound) {
-    // if the status argument is not defined in the httpResConfig, create a custom response
-    const payload =
-      200 <= status && status < 300
-        ? { success: statusText }
-        : { error: statusText };
-
-    return NextResponse.json(
-      {
-        ...payload,
-      },
-      { status },
-    );
+  let statusTextVal;
+  if (statusText.length > 0) {
+    statusTextVal = statusText;
+  } else if (httpResConfigKey) {
+    statusTextVal = httpResConfig[httpResConfigKey]['statusText'];
   }
 
-  // if the status argument is defined in the httpResConfig
-  const [httpResConfigKey] = result;
+  // NextResponse will assign '' empty string to statusText by default
 
-  // console.log('httpResConfigKey====>', httpResConfigKey);
-
-  return httpRes[httpResConfigKey]();
-};
+  return NextResponse.json(
+    { data, message },
+    { status, statusText: statusTextVal, headers },
+  );
+}
 
 // still needed, even though, we have httpResByStatus, since catched errors don't have a status code
-function serverResponseError(error: unknown) {
-  // console.error(
-  //   JSON.stringify(error, ['name', 'message', 'stack']),
-  // );
+function serverResponseError(error: unknown, options?: { message: string }) {
+  const optionsMsg = options?.message;
+  if (optionsMsg) console.log(optionsMsg);
 
-  console.error(error);
+  const { message: errorMsg, name: errorName } = error as Error;
+  const errorMsgWithName = `[${errorName}]: ${errorMsg}`;
+  console.error(new Error(errorMsgWithName));
 
-  // any error that extends Error (e.g. TypeError, NeonDbError) will also be true)
-  // need this conditional, otherwise typescript error for error.message since error params type is unknown
-  if (error instanceof Error) {
-    // console.log(
-    //   '[ERROR]:',
-    //   '\nName: ',
-    //   error.name,
-    //   '\nMessage: ',
-    //   error.message,
-    //   '\nStack: ',
-    //   error.stack,
-    //   '\nCause: ',
-    //   error.cause,
-    // );
+  // any error that extends Error will be an instanceof it
+  // possible error types: TypeError, NeonDbError, DrizzleError
+  // throw new Error('Something went wrong on the server side.');
+  // better to send a response since we can send additional info in response body
 
-    // console.log('error check', error instanceof TypeError);
-
+  if (error instanceof DrizzleError && errorMsg.includes('Rollback')) {
     return httpRes.internalServerErr({
-      // message: `${error.name}: ${error.message}`, // don't show error.name in response body
-      // SHOULD WE REALLY BE PASSING MESSAGES TO THE USER?
-      // TODO: MAYBE JUST USE error.message ? 'Something went wrong in the server.' : 'Unknown Server Error'
-      message: error.message ?? 'Unknown Server Error',
+      message: optionsMsg ?? `${errorMsgWithName} (Transaction failed)`,
     });
   }
 
-  //  no need for this since error object will always be an Error type
-  // return httpRes.internalServerErr({
-  //   message: 'Unknown Server Error',
-  // });
+  const responseMsg = optionsMsg ?? errorMsgWithName;
+
+  // no need to handle if an error instance, since this fn will only be used in a catch block where an error object is received
+
+  return httpRes.internalServerErr({
+    message: responseMsg ?? 'Unknown Server Error',
+  });
+
+  // console.error(JSON.stringify(error, ['name', 'message', 'stack']));
+  // console.log(error.name, error.message, error.stack, error.cause);
 }
 
 export { httpResConfig, httpRes, httpResByStatus, serverResponseError };
-
-// NOT NEEDED
-// interface SuccessResponse {
-//   // success: string; // since already provided by the config | only passed args need to be specified in the type
-
-//   data: unknown;
-//   message?: string;
-// }
-
-// interface ErrorResponse {
-//   // error: string;
-//   message?: string;
-// }
-
-// interface HttpRes {
-//   [key: string]: (
-//     payload: SuccessData | ErrorData,
-//   ) => NextResponse<SuccessResponse | ErrorResponse>;
-// }
